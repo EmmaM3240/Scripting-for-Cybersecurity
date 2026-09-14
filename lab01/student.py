ae57bb0ada1da46ed5375035bc2d19fd080b7638
EmmaM3240 <c00323240@setu.ie>
2026-09-14 11:39:27 +0000

    Complete Lab 1 - Student Information

---
+++ lab01/student.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+number = input("What is your student number? ")
+year = input ("What is your year of study? ")
+print ("Your name is " + name + ", your student number is " + number + ", and your year of study is " + year + ".")
