ae9379e1d9f418eed9f016aee1c2341b31d9c152
EmmaM3240 <c00323240@setu.ie>
2026-09-14 11:32:22 +0000

    update my code

--- lab01/hello.py
+++ lab01/hello.py
@@ -1,1 +1,9 @@
 print("Hello World")
+name = input("What is your name? ")
+print("Hello, " + name + "! ")
+course = input("What course are you Studying? ")
+area = input("What is your favourite area of study in your course? ")
+print("You are studying " + course + ".")
+print("Your favourite area of study is " + area + ".")
+print ("your name is " + name + ", you are studying " + course + " and your favourite area of study is " + area + ".")
+
